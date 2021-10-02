e1811cbf24e8f8e19527bfe193826f9c45b115f4
Glyde <anton123454321045@yandex.by>
2021-10-02 23:24:39 +0300

    Get status of pages

--- DLL/dllmain.cpp
+++ DLL/dllmain.cpp
@@ -20,32 +20,40 @@ extern "C" {          // we need to export the C interface
 
     __declspec(dllexport) int __cdecl myPuts(LPWSTR lpszMsg)
     {
-        DWORD cchWritten;
-        HANDLE hConout;
-        BOOL fRet;
+        //DWORD cchWritten;
+        //HANDLE hConout;
+        //BOOL fRet;
+
+
+        MEMORY_BASIC_INFORMATION memInfo;
+
+        HANDLE hProcess = GetCurrentProcess();
+        SYSTEM_INFO lpSysInfo;
+        GetSystemInfo(&lpSysInfo);
+        VirtualQueryEx(hProcess, &lpSysInfo, &memInfo, sizeof(memInfo));
 
         // Get a handle to the console output device.
 
-        hConout = CreateFileW(L"CONOUT$",
-            GENERIC_WRITE,
-            FILE_SHARE_WRITE,
-            NULL,
-            OPEN_EXISTING,
-            FILE_ATTRIBUTE_NORMAL,
-            NULL);
+        //hConout = CreateFileW(L"CONOUT$",
+        //    GENERIC_WRITE,
+        //    FILE_SHARE_WRITE,
+        //    NULL,
+        //    OPEN_EXISTING,
+        //    FILE_ATTRIBUTE_NORMAL,
+        //    NULL);
 
-        if (INVALID_HANDLE_VALUE == hConout)
-            return EOF;
+        //if (INVALID_HANDLE_VALUE == hConout)
+        //    return EOF;
 
-        // Write a null-terminated string to the console output device.
+        //// Write a null-terminated string to the console output device.
 
-        while (*lpszMsg != L'\0')
-        {
-            fRet = WriteConsole(hConout, lpszMsg, 1, &cchWritten, NULL);
-            if ((FALSE == fRet) || (1 != cchWritten))
-                return EOF;
-            lpszMsg++;
-        }
+        //while (*lpszMsg != L'\0')
+        //{
+        //    fRet = WriteConsole(hConout, lpszMsg, 1, &cchWritten, NULL);
+        //    if ((FALSE == fRet) || (1 != cchWritten))
+        //        return EOF;
+        //    lpszMsg++;
+        //}
         return 1;
     }
 
--- StaticDLL/Source.c
+++ StaticDLL/Source.c
@@ -1,4 +1,5 @@
 #include <windows.h> 
+#include <stdio.h>
 
 extern int __cdecl myPuts(LPWSTR);   // a function from a DLL
 
@@ -6,6 +7,45 @@ int main(VOID)
 {
     int Ret = 1;
 
-    Ret = myPuts(L"Message sent to the DLL function\n");
+    //Ret = myPuts(L"Message sent to the DLL function\n");
+
+    MEMORY_BASIC_INFORMATION memInfo;
+
+    HANDLE hProcess = GetCurrentProcess();
+    SYSTEM_INFO lpSysInfo;
+    GetSystemInfo(&lpSysInfo);
+
+
+    DWORD start = lpSysInfo.lpMinimumApplicationAddress;
+
+    
+
+
+    while (start < lpSysInfo.lpMaximumApplicationAddress)
+    {
+        VirtualQueryEx(hProcess, start, &memInfo, sizeof(memInfo));
+        DWORD err = GetLastError();
+
+        switch (memInfo.AllocationProtect) {
+        case PAGE_READONLY:
+            printf("%s%d\n", "READONLY  ", memInfo.BaseAddress);
+            break;
+        case PAGE_READWRITE:
+            printf("%s%d\n", "READWRITE  ", memInfo.BaseAddress);
+            break;
+        case PAGE_NOACCESS:
+            printf("%s%d\n", "NOACCESS  ", memInfo.BaseAddress);
+            break;
+        }
+        start += memInfo.RegionSize;
+    }
+
+
+
+
+    if (memInfo.Protect == PAGE_READWRITE)
+    {
+        wsprintf("%s", L"READWRITE");
+    }
     return Ret;
 }
