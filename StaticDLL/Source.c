4ef54ddf56ea0a89010990e3ccb9ff8387ec4786
Glyde <anton123454321045@yandex.by>
2021-10-04 00:31:13 +0300

    trying to read the memory

--- StaticDLL/Source.c
+++ StaticDLL/Source.c
@@ -9,6 +9,13 @@ int main(VOID)
 
     //Ret = myPuts(L"Message sent to the DLL function\n");
 
+    #define MY_BUFSIZE 1024 // Buffer size for console window titles.
+    char pszOldWindowTitle[MY_BUFSIZE]; // Contains original
+                                        // WindowTitle.
+    GetConsoleTitle(pszOldWindowTitle, MY_BUFSIZE);
+    HWND hwnd = FindWindowA(NULL, pszOldWindowTitle);
+
+
     MEMORY_BASIC_INFORMATION memInfo;
 
     HANDLE hProcess = GetCurrentProcess();
@@ -18,34 +25,70 @@ int main(VOID)
 
     DWORD start = lpSysInfo.lpMinimumApplicationAddress;
 
+    int lpBuffer = 0;
     
 
+    DWORD ProcId; // ѕеременна€ хран€ща€ в себе ID процесса
+    GetWindowThreadProcessId(hwnd, &ProcId); // получение ID процесса
+    HANDLE hChild = OpenProcess(PROCESS_ALL_ACCESS, FALSE, ProcId); // handle будет открывать дл€ вашей программы вход в окно (если что поправьте)
+
+    const char* signature = "DLL.exe";      // ѕоследовательность которую будем искать
+    size_t len_signature = strlen(signature); // ƒлина последовательности.
+
+    char dll[7] = { 'D', 'L','L','.','e','x', 'e' };
 
     while (start < lpSysInfo.lpMaximumApplicationAddress)
     {
         VirtualQueryEx(hProcess, start, &memInfo, sizeof(memInfo));
         DWORD err = GetLastError();
 
-        switch (memInfo.AllocationProtect) {
-        case PAGE_READONLY:
-            printf("%s%d\n", "READONLY  ", memInfo.BaseAddress);
-            break;
-        case PAGE_READWRITE:
-            printf("%s%d\n", "READWRITE  ", memInfo.BaseAddress);
-            break;
-        case PAGE_NOACCESS:
-            printf("%s%d\n", "NOACCESS  ", memInfo.BaseAddress);
-            break;
+        //switch (memInfo.AllocationProtect) {
+        //case PAGE_READONLY || PAGE_NOACCESS:
+        //    printf("%s%d\n", "READONLY  or NOACCESS", memInfo.BaseAddress);
+        //    break;
+        //default:
+        //    
+        //   // ReadProcessMemory(hChild, memInfo.BaseAddress, &lpBuffer, memInfo.RegionSize, NULL);
+        //    ReadProcessMemory(hChild, memInfo.BaseAddress, &lpBuffer, sizeof(lpBuffer), 0); // читаем наш адресс из пам€ти
+        //    if (lpBuffer != 0)
+        //        printf("%d\n", lpBuffer);
+        //    break;
+        //}
+
+        if (memInfo.AllocationProtect != (PAGE_READONLY || PAGE_NOACCESS))
+        {
+            if (memInfo.RegionSize != 0)
+            {
+                BYTE* lpData = (BYTE*)malloc(memInfo.RegionSize);//резервируем место дл€ массива lpData, размером в регион
+
+                if (ReadProcessMemory(hChild, (LPCVOID)start, lpData, memInfo.RegionSize, 0))
+                {
+                    
+
+                    BYTE* p = lpData;
+                    while ((p = (BYTE*)memchr(p, signature[0], memInfo.RegionSize - len_signature + 1 - (p - lpData))) != 0)
+                    {
+                        if (0 == memcmp(p + 1, signature + 1, len_signature - 1))
+                        {
+                            // найдено!!! в (p) адрес нашего буфера, где обнаружена последовательность.
+                            // соответственно адрес в приложении равен: Start + (p - lpData);
+                            printf("%d  --  %d\n", lpData, start);
+
+                            //WriteProcessMemory(hChild, start + (p - lpData), L"Helloee", sizeof("helloee"), 0);
+
+
+                        }
+                        ++p;
+                    }
+
+                }
+                free(lpData);
+            }
         }
+
         start += memInfo.RegionSize;
     }
 
-
-
-
-    if (memInfo.Protect == PAGE_READWRITE)
-    {
-        wsprintf("%s", L"READWRITE");
-    }
+    
     return Ret;
 }
